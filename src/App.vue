1c812bf12e3ec9a55ea33797d50bdf173a893a82
daviddennis02 <dennis.david02@gmail.com>
2020-09-30 04:18:58 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeaderComponent />
+    <div>Content</div>
+    <FooterComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderComponent from "./components/Header-Footer/HeaderComponent.vue";
+import FooterComponent from "./components/Header-Footer/FooterComponent.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { HeaderComponent, FooterComponent },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style.css";
 </style>
---
+++ src/components/Header-Footer/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="container clear">
+      <div class="logo">logo</div>
+      <div class="nav">nav</div>
+    </div>
+  </header>
+</template>
+
+<script>
+import Logo from '../../assets/images/logo.png';
+import Menu from '../../assets/images/menu.png';
+
+export default {
+    name: 'HeaderComponent',
+    components:{},
+    data(){
+        return{
+            LogoIcon: Logo,
+            MenuIcon: Menu
+        }
+    }
+};
+</script>
+
+<style  scoped>
+
+</style>
